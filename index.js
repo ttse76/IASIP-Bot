90424cfb6abcb218f5db1befe5ca693b3a569b75
ttse76 <timtse76@gmail.com>
2020-10-13 23:24:33 -0400

    frank audio connected

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const{
 } = require('./config.json');
 
 const client = new Discord.Client();
+const frank = ['suicide', 'pure', 'kids'];
+
 client.login(token);
 
 client.once('ready', () => {
@@ -17,4 +19,56 @@ client.once('reconnecting', () => {
 
 client.once('disconnecting', () => {
     console.log("Disconnecting :(");
+});
+
+client.on('message', async message => {
+    if(message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    const voiceChannel = message.member.voice.channel;
+    if(!voiceChannel){
+        return message.channel.send("You need to be in a voice channel");
+    }
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+        return message.channel.send(
+            "Bot requires permissions to join and speak in your voice channel"
+        );
+    }
+    if(!message.content.startsWith(prefix)) return;
+
+    if(command.startsWith('frank')){
+        if(!args.length){
+            var quote = frank[Math.floor(Math.random() * frank.length)];
+            playQuote('frank', quote);
+        }
+        else{
+            var quote = args[0].toLowerCase();
+            if(frank.includes(quote)){
+                playQuote('frank', quote);
+            }
+            else{
+                message.channel.send('Invalid selection');
+            }
+        }
+    }else{
+        message.channel.send('character not valid');
+    }
+
+    function playQuote(character, quote){
+        var quoteFile = 'audio/' + character + '/' + quote + '.mp3';
+        
+        try{
+            voiceChannel.join().then(connection => {
+                connection.play(quoteFile).on('finish', () => {
+                    voiceChannel.leave();
+                });
+                
+            });
+            
+        }catch(err){
+            return message.channel.send(err);
+        }
+    }
 });
